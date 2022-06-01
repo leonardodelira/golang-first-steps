6fe5ac2ba570b4b4da507a3b1b6fd412fc98f422
leonardodelira <leonardo.lira@hotmail.com.br>
2022-06-01 16:37:55 -0300

    json marshal

---
+++ Json/1-json-marshal.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Name struct {
+	Name string `json:"name"`
+	Document string `json:"document"`
+}
+
+func main() {
+	person := Name{"Leonardo", "458.000.123-87"}
+	fmt.Println(person)
+
+	m, err := json.Marshal(person)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(m) //show bytes
+
+	fmt.Println(string(m)) //show string json
+}
